349a43f2ce242fd4dfe93486846f32c82b5327ba
etheoo <etheoo@protonmail.com>
2023-11-08 15:47:00 +0100

    Cleaning stuff

--- VirtualBookingSystem/Ebook.cs
+++ VirtualBookingSystem/Ebook.cs
@@ -1,17 +1,17 @@
 namespace VirtualBookingSystem
 {
-    public class Ebok:Bok
+    public class Ebook:Book
     {
         double FileSize { get; set; }
         string DownloadUrl { get; set; }
 
-        public Ebok(string title,string author,string isbn, double fileSize, string downloadUrl) : base(title, author, isbn)
+        public Ebook(string title,string author,string isbn, double fileSize, string downloadUrl) : base(title, author, isbn)
         {
             FileSize = fileSize;
             DownloadUrl = downloadUrl;
         }
 
-        public virtual override void printInfo()
+        public override void PrintInfo()
         {
             Console.WriteLine($"Title:{Title}\nAuthor:{Author}\nISBN{ISBN}\nFileSize:{FileSize}\nDownloadUrl:{DownloadUrl}");
         }
