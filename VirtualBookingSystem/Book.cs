7a0a6ffa702438081b3e092081e82b6e9bd2ea45
Yarub1 <yarub.a@outlook.com>
2023-11-08 14:46:29 +0100

    ::::

---
+++ VirtualBookingSystem/Book.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualBookingSystem
+{
+    public class Book
+    {
+        public string Titel { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+
+        public Book(string titel, string author, string isbn)
+        {
+            Titel = titel;
+            Author = author;
+            ISBN = isbn;
+        }
+
+        public virtual void SkrivUtInfo()
+        {
+            Console.WriteLine("Bokdetaljer:");
+            Console.WriteLine($"Titel: {Titel}");
+            Console.WriteLine($"Författare: {Author}");
+            Console.WriteLine($"ISBN: {ISBN}");
+        }
+    }
+}
